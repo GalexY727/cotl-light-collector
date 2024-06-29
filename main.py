588f03d3303fb6f1a2086297b906eb006ad7ae11
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-06-29 14:07:25 -0700

    fix locations and organize key pressing, also create double clicking for those who lit your candle back

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ friend_count = 0
 
 def press_key(key):
     try:
-        pyautogui.locateOnScreen('./media/esc.png')
+        pyautogui.locateOnScreen('./media/esc.png', grayscale=1, region=(1765, 1026, 120, 50), confidence=.7)
         ahk.key_down(key)
         time.sleep(.1)
         ahk.key_up(key)
@@ -26,7 +26,7 @@ def press_key(key):
 
 def index_to_start():
     try:
-        pyautogui.locateOnScreen('./media/add_friends.png')
+        pyautogui.locateOnScreen('./media/add_friends.png', grayscale=1, confidence=confidence)
         return
     except:
         ahk.key_press('z')
@@ -45,16 +45,16 @@ def make_unique_list(list, pixel_distance=3):
 
 # run AFTER we index or else everything explodes
 def get_total_pages():
-    left, top, width, height = pyautogui.locateOnScreen('./media/add_friends.png')
+    left, top, width, height = pyautogui.locateOnScreen('./media/add_friends.png', grayscale=1, confidence=confidence)
     screen_width, screen_height = pyautogui.size()
     try:
-        positions = pyautogui.locateAllOnScreen('./media/page_bubble_online.png')
+        positions = pyautogui.locateAllOnScreen('./media/page_bubble_online.png', grayscale=0, region=(left, top+height, 220, screen_height-(top+height)), confidence=confidence)
         pages_online = positions
     except:
         pages_online = 0
         print("No online")
     try:
-        positions = pyautogui.locateAllOnScreen('./media/page_bubble.png')
+        positions = pyautogui.locateAllOnScreen('./media/page_bubble.png', grayscale=0, region=(left, top+height, 220, screen_height-(top+height)), confidence=confidence)
         pages_offline = positions
     except:
         pages_offline = 0
@@ -64,21 +64,25 @@ def get_total_pages():
 def get_friends_on_current_page():
     pyautogui.moveTo(500, 0)
     try:
-        return list(pyautogui.locateAllOnScreen('./media/unlit_friend.png'))
+        return list(pyautogui.locateAllOnScreen('./media/unlit_friend.png', grayscale=1, region=(190, 160, 1691, 948), confidence=confidence))
     except:
         return [] 
 
 def lightFriend(x, y):
     pyautogui.click(x, y)
     time.sleep(.5) # we need this bc the q kinda stays there for a bit
+    try:
+        q_position = pyautogui.locateOnScreen('./media/q_info.png', grayscale=1, region=(1540, 1025, 30, 30), confidence=confidence)
+        print("Double clicking")
+        pyautogui.click(x, y)
+        pyautogui.moveTo(q_position)
+    except:
+        pass
     time.sleep(.9)
-    ahk.key_down('f')
-    time.sleep(.1)
-    ahk.key_up('f')
+    print("Hitting f")
+    press_key('f')
     time.sleep(.3)
-    ahk.key_down('esc')
-    time.sleep(.1)
-    ahk.key_up('esc')
+    press_key('esc')
     time.sleep(.3)
     global friend_count
     friend_count += 1
@@ -94,10 +98,7 @@ def loop():
             for friend in friends:
                 x, y = pyautogui.center(friend)
                 lightFriend(x, y)
-        # next page
-        ahk.key_down('c')
-        time.sleep(.1)
-        ahk.key_up('c')
+        press_key('c')
         time.sleep(page_transition_time)
         current_page += 1
 
