5e94ffdafb5b09293749abf0d7326c282069c2ca
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-06-29 13:03:08 -0700

    Make it so that it can see both online and offline pages

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ import time
 ahk = AHK()
 
 page_transition_time = 0.86
+confidence = 0.9
+
+total_pages = -1
+current_page = 0
+
 friend_count = 0
+
+def press_key(key):
+    try:
+        pyautogui.locateOnScreen('./media/esc.png')
+        ahk.key_down(key)
+        time.sleep(.1)
+        ahk.key_up(key)
+    except:
+        print("Esc not found")
+        pass
 
 def index_to_start():
     try:
@@ -17,9 +32,34 @@ def index_to_start():
         ahk.key_press('z')
         index_to_start()
 
+def make_unique_list(list, pixel_distance=3):
+    unique_list = []
+    for item in list:
+        unique = True
+        for unique_item in unique_list:
+            if abs(unique_item[0] - item[0]) < pixel_distance:
+                unique = False
+        if unique:
+            unique_list.append(item)
+    return unique_list
+
 # run AFTER we index or else everything explodes
 def get_total_pages():
-    return len(list(pyautogui.locateAllOnScreen('./media/page_bubble.png')))
+    left, top, width, height = pyautogui.locateOnScreen('./media/add_friends.png')
+    screen_width, screen_height = pyautogui.size()
+    try:
+        positions = pyautogui.locateAllOnScreen('./media/page_bubble_online.png')
+        pages_online = positions
+    except:
+        pages_online = 0
+        print("No online")
+    try:
+        positions = pyautogui.locateAllOnScreen('./media/page_bubble.png')
+        pages_offline = positions
+    except:
+        pages_offline = 0
+        print("No offline")
+    return len(make_unique_list(list(pages_online) + list(pages_offline)))
 
 def get_friends_on_current_page():
     pyautogui.moveTo(500, 0)
@@ -47,12 +87,13 @@ def loop():
     global current_page
     while current_page <= total_pages:
         # this loop helps double check missed friends
-        friends = get_friends_on_current_page()
-        if len(friends) == 0:
-            break
-        for friend in friends:
-            x, y = pyautogui.center(friend)
-            lightFriend(x, y)
+        while True:
+            friends = make_unique_list(get_friends_on_current_page(), pixel_distance=10)
+            if len(friends) == 0:
+                break
+            for friend in friends:
+                x, y = pyautogui.center(friend)
+                lightFriend(x, y)
         # next page
         ahk.key_down('c')
         time.sleep(.1)
