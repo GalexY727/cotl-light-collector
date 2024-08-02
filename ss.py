548c52519ff502597dbd26713a57cf93c59bddd9
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-08-02 16:18:04 -0700

    Add recovery feature to re-enter the ui if it misses

--- ss.py
+++ ss.py
@@ -1,4 +1,6 @@
 import pyautogui
+import time
 
-im1 = pyautogui.screenshot(region=(1765, 1026, 120, 50))#region=(150,800,1200,280))
+time.sleep(1)
+im1 = pyautogui.screenshot(region=(1070, 762, 81, 93))#region=(150,800,1200,280))
 im1.save(r"./region.png")
